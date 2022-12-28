b84d426b027fd82c560f25edd946da4d38f04760
michaelcchu <50280000+michaelcchu@users.noreply.github.com>
2022-12-28 16:49:52 -0500

    added spacing, disabled offer when text is in box

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const answer = document.getElementById("answer");
 const text = document.getElementById("text");
 const chat = document.getElementById("chat");
 const display = document.getElementById("display");
-const config = {iceServers: [{urls: "stun:stun.1.google.com:19302"}]};
+const config = { iceServers: [ { urls: "stun:stun.1.google.com:19302" } ] };
 const pc = new RTCPeerConnection(config);
 const dc = pc.createDataChannel("chatchannel", {negotiated: true, id: 0});
 dc.addEventListener("message", (e) => { 
   display.innerHTML += "> " + e.data + "\r\n"; 
 });
 pc.addEventListener("icecandidate", ({candidate}) => {
-  if (candidate) return;
+  if (candidate) { return; }
   offer.disabled = true;
   display.innerHTML = pc.localDescription.sdp;
 });
@@ -18,16 +18,17 @@ offer.addEventListener("click", () => {
   pc.setLocalDescription(pc.createOffer());
 });
 answer.addEventListener("click", () => {
-  answer.disabled = true;
+  answer.disabled = text.disabled = true;
   if (pc.signalingState === "stable") {
-    pc.setRemoteDescription({type: "offer", sdp: text.value.trim()+"\r\n"});
+    pc.setRemoteDescription({type: "offer", sdp: text.value.trim() + "\r\n"});
     pc.setLocalDescription(pc.createAnswer());
   } else {
-    pc.setRemoteDescription({type: "answer", sdp: text.value.trim()+"\r\n"});
+    pc.setRemoteDescription({type: "answer", sdp: text.value.trim() + "\r\n"});
   }
 })
+text.addEventListener("input", () => { offer.disabled = true; });
 chat.addEventListener("keydown", (e) => {
-  if (e.key!=="Enter") {return;}
+  if (e.key !== "Enter") { return; }
   dc.send(chat.value);
   display.innerHTML += chat.value + "\r\n";
   chat.value = "";
